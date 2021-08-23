366da70af2ba11c5fcae754b45fbddf2fb4ddb5b
VITALIY1986 <staslego1986@gmail.com>
2021-08-23 14:00:54 +0300

    asd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import Nav from "./Nav";
-
+import React, { useState, useEffect } from "react"
 const Header = () => {
+	const [scroll, setScroll] = useState(false);
+	useEffect(() => {
+	  window.addEventListener("scroll", () => {
+		setScroll(window.scrollY > 100);
+	  });
+	}, []);
 	return (
-		<div className="header">
+		<div className={scroll ? "bg-white w-full fixed z-50 duration-700 py-0" : " duration-700 py-2"}>
 			<Nav/>
 		</div>
 	)
