b770165c0f10192f8e44e2bd834ed90e37e58ae4
VITALIY1986 <staslego1986@gmail.com>
2021-12-06 17:29:38 +0200

    asd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ const Footer = () => (
 		<div className="container mx-auto">
 			<div className="md:grid  md:grid-cols-6 gap-4 ">
 				<div className="col-start-1 col-end-4 ">
-					<h3 className="mt-5 text-3xl">КОНТАКТЫ</h3>
+					<h3 className="mt-5 text-3xl text-blue">КОНТАКТЫ</h3>
 					<div className="flex-none md:flex mt-5">
-						<div className="flex-1" >
+						<div className="flex-1 opacity-75" >
 					     <p className="mb-3">Chantarelle</p>
 					     <p className="mb-3">Norel</p>
 					     <p className="mb-3">Dermaoxy</p>
 						 </div>
-						 <div className="flex-1">
+						 <div className="flex-1 opacity-75">
 					     <p className="mb-3">Arkana</p>
 					     <p className="mb-3">Beyond Dental </p>
 					     <p className="mb-3">Sittara</p>
@@ -20,8 +20,8 @@ const Footer = () => (
 					</div>
 			    </div>
 				<div className="col-start-4 col-end-6 ">
-					<h3 className="mt-5 text-3xl">КОНТАКТЫ</h3>
-					<div className="mt-5">
+					<h3 className="mt-5 text-3xl text-blue">КОНТАКТЫ</h3>
+					<div className="mt-5 opacity-75">
 						<p>Mенеджер (Киев): +38 (097) 860 06 06 </p>
 						<p>Mенеджер (Киев): +38 (068) 706 70 67 </p>
 						<p>Mенеджер (регионы): +38 (097) 209 54 45</p>
@@ -31,8 +31,8 @@ const Footer = () => (
 					</div>
 				</div>
 				<div className="col-start-6 col-end-6">
-				     <h3 className="mt-5 text-3xl">МЫ В СОЦ.СЕТЯХ</h3>
-				     <ul className="social-links flex  mt-5">
+				     <h3 className="mt-5 text-3xl text-blue">МЫ В СОЦ.СЕТЯХ</h3>
+				     <ul className="social-links flex  mt-5 ">
 				        <li><a href="https://www.facebook.com/4ProfUkraine" className="" target="_blank"><Facebook/></a></li>
 				        <li className="ml-2 mt-1"><a href="#" target="_blank"><Twitter/></a></li>
 				        <li className="ml-2 mt-1"><a href="https://www.youtube.com/channel/UC3BBKd1UUUtXMjY4uL_NZ4g" className="fa fa-youtube" target="_blank"><Youtube/></a></li>
