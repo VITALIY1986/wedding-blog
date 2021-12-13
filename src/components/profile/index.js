a74a78dbd2093ea5f2bd4e58e685a67c3a1e116c
VITALIY1986 <staslego1986@gmail.com>
2021-12-13 18:47:09 +0200

    asd

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,18 +1,72 @@
 /**
  * Internal dependencies
  */
+ import { useQuery, gql, ApolloError } from "@apollo/client";
+ import client from '../context/AppContext';
  import { useAuth } from '../login-function/hooks';
-
- 
- export const Profile = () => {
+ import PRODUCTS_AND_CATEGORIES_QUERY from "../../queries/product-and-categories";
+ import Product from "../../components/Product";
+ 	 export const GET_USER = gql`
+	 query getUser {
+	    products(first: 5, where: {visibility: VISIBLE, sku: "CD1622"}) {
+			nodes {
+			  id
+			  productId: databaseId
+			  averageRating
+			  slug
+			  description
+			  sku
+			  image {
+				id
+				altText
+				sourceUrl
+			  }
+			  name
+			  ... on SimpleProduct {
+				price
+				regularPrice
+				id
+			  }
+			  ... on VariableProduct {
+				price
+				id
+				regularPrice
+			  }
+			  ... on ExternalProduct {
+				price
+				id
+				externalUrl
+				regularPrice
+			  }
+			  ... on GroupProduct {
+				id
+				products {
+				  nodes {
+					... on SimpleProduct {
+					  id
+					  price
+					  regularPrice
+					}
+				  }
+				}
+			  }
+			}
+		  }
+	 }
+   `;
+ export const Profile = ({ children }) => {
+	const { data, loading, error } = useQuery(GET_USER);
 	 const { viewer, loadingViewer, logout } = useAuth();
- 
-	
+
+	 const products = data?.products.nodes;
+	 
+
 	 return (
 		 <div className="profile">
 			 <hgroup>
 				 <h2>Account</h2>
 				 <div>
+				
 					 <p>
 						 Edit your account details below, or{ ' ' }
 						 <button onClick={ logout } className="link-button">
