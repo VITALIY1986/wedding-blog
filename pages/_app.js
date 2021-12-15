e1be3c4dddac5da5846aa3a0c5de5c51d851b50d
VITALIY1986 <staslego1986@gmail.com>
2021-12-15 13:55:51 +0200

    asd

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import "../src/styles/main.scss";
 import { ApolloProvider } from "@apollo/client";
 import Router from 'next/router';
 import NProgress from 'nprogress';
-import client from "./registration/ApolloClient";
-import { AppProvider } from "./registration/AppContext";
+import client from "../src/components/ApolloClient";
+import { AppProvider } from "../src/components/context/AppContext";
 import { AuthContextProvider } from '../src/components/login-function/auth-context';
 NProgress.configure({ showSpinner: false });
 Router.events.on('routeChangeStart', () => NProgress.start());
@@ -13,6 +13,9 @@ Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   
   return <AuthContextProvider><AppProvider><ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider></AppProvider></AuthContextProvider>
+
+
+
 }
 
 export default MyApp
